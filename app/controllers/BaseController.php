84214917c6452165cbc94cc3c56a8c00d122a30f
you-d <ykwanmas@gmail.com>
2015-06-29 13:05:20 +1000

    Modified timezone
    Modified the timezone through the config/app.php instead of using the
    date_default_timezone_set() in BaseController.php

--- app/controllers/BaseController.php
+++ app/controllers/BaseController.php
@@ -1,15 +1,16 @@
 <?php
-date_default_timezone_set('Australia/Melbourne');
+// Check config/app.php to modify the web app's timezone
+//date_default_timezone_set('Australia/Melbourne');
 
 abstract class BaseController extends Controller {
 	protected $xmlOutputArray = null;
 	protected $cacheTimeLimit = 1440; // 24 hrs
-		
+
 	private $featuresModuleTotEntries = 0;
 	private $newsModuleTotEntries = 0;
 	private $rensaiModuleTotEntries = 0;
 	private $gadgetsModuleTotEntries = 0;
-	
+
 	/**
 	 * Setup the layout used by the controller.
 	 *
@@ -22,40 +23,40 @@ abstract class BaseController extends Controller {
 			$this->layout = View::make($this->layout);
 		}
 	}
-		
+
 	/* Abstract functions */
 	abstract public function getViewName($pageType);
 	abstract public function getAlias();
 	abstract public function getXmlNodeLabel();
 	abstract public function redirectBack();
-	
+
 	/* Determine the name of the folder of the cms view files */
 	protected function getCmsViewFolder() {
 		// cms/sample-cms-page.blade.php
 		return "cms" . DIRECTORY_SEPARATOR;
-	} 
-	
-	/* 
+	}
+
+	/*
 		Possible parameter values:
 		$page -> "home"
-		$column -> "headerimg" -> $val = array([wide img filename], [narrow img filename]) , 
-				   "headline" -> $val = array([entry idx (0-4)], [record type (features, rensai, etc...)], [record id]), 
+		$column -> "headerimg" -> $val = array([wide img filename], [narrow img filename]) ,
+				   "headline" -> $val = array([entry idx (0-4)], [record type (features, rensai, etc...)], [record id]),
 				   "features-highlight" -> $val = (int) [total entries],
-				   "news" -> $val = (int) [total entries], 
-				   "rensai" -> $val = (int) [total entries], 
-				   "gadgets" -> $val = (int) [total entries]   
-	*/	
+				   "news" -> $val = (int) [total entries],
+				   "rensai" -> $val = (int) [total entries],
+				   "gadgets" -> $val = (int) [total entries]
+	*/
 	protected function setXmlConfig($page, $column, $val) {
 		$xmlConfigObj = new XmlConfig($page, $this->cacheTimeLimit);
 		$xmlConfigObj->setThisPageConfig($column, $val);
 	}
-	
+
 	/* Get the setting values from the xml config file */
-	protected function getXmlConfig($page) { 
+	protected function getXmlConfig($page) {
 		$xmlConfigObj = new XmlConfig($page, $this->cacheTimeLimit);
 		$this->xmlOutputArray = $xmlConfigObj->getThisPageConfig();
-		
-		// get the total entries for each module 
+
+		// get the total entries for each module
 		if (array_key_exists("featuresModuleTotalEntries", $this->xmlOutputArray)) {
 			$this->featuresModuleTotEntries = $this->xmlOutputArray["featuresModuleTotalEntries"];
 		}
@@ -63,20 +64,20 @@ abstract class BaseController extends Controller {
 			$this->newsModuleTotEntries = $this->xmlOutputArray["newsModuleTotalEntries"];
 		}
 		if (array_key_exists("rensaiModuleTotalEntries", $this->xmlOutputArray)) {
-			$this->rensaiModuleTotEntries = $this->xmlOutputArray["rensaiModuleTotalEntries"]; 
+			$this->rensaiModuleTotEntries = $this->xmlOutputArray["rensaiModuleTotalEntries"];
 		}
 		if (array_key_exists("gadgetsModuleTotalEntries", $this->xmlOutputArray)) {
 			$this->gadgetsModuleTotEntries = $this->xmlOutputArray["gadgetsModuleTotalEntries"];
 		}
 	}
-	
+
 	protected function getFeaturesModuleEntries() {
 		if (Cache::has('module-features')) {
 			$moduleEntries = Cache::get('module-features');
 		} else {
 			$moduleEntries = DB::table('feature_post')->
-								select('feature_post.post_id', 'feature_post.posting_date', 
-									   'feature_post.post_title', 'feature_post.category_id', 
+								select('feature_post.post_id', 'feature_post.posting_date',
+									   'feature_post.post_title', 'feature_post.category_id',
 									   'feature_post.thumbnail_img', 'feature_category.category_name')->
 								join('feature_category', 'feature_post.category_id', '=', 'feature_category.id')->
 								orderBy('feature_post.id','desc')->
@@ -84,11 +85,11 @@ abstract class BaseController extends Controller {
 								get();
 			Cache::add('module-features', $moduleEntries, $this->cacheTimeLimit);
 		}
-		
+
 		return $moduleEntries;
 	}
-	
-	protected function getNewsModuleEntries() {				
+
+	protected function getNewsModuleEntries() {
 		if (Cache::has('module-news')) {
 			$moduleEntries = Cache::get('module-news');
 		} else {
@@ -99,17 +100,17 @@ abstract class BaseController extends Controller {
 								get();
 			Cache::add('module-news', $moduleEntries, $this->cacheTimeLimit);
 		}
-		
-		return $moduleEntries;		
-	}	
-	
-	protected function getRensaiModuleEntries() {				
+
+		return $moduleEntries;
+	}
+
+	protected function getRensaiModuleEntries() {
 		if (Cache::has('module-rensai')) {
 			$moduleEntries = Cache::get('module-rensai');
 		} else {
 			$moduleEntries = DB::table('rensai_post')->
-								select('rensai_post.post_id', 'rensai_post.posting_date', 
-									   'rensai_post.post_title', 'rensai_post.category_id', 
+								select('rensai_post.post_id', 'rensai_post.posting_date',
+									   'rensai_post.post_title', 'rensai_post.category_id',
 									   'rensai_post.thumbnail_img', 'rensai_category.category_name')->
 								join('rensai_category', 'rensai_post.category_id', '=', 'rensai_category.id')->
 								orderBy('rensai_post.id','desc')->
@@ -117,11 +118,11 @@ abstract class BaseController extends Controller {
 								get();
 			Cache::add('module-rensai', $moduleEntries, $this->cacheTimeLimit);
 		}
-		
-		return $moduleEntries;			
+
+		return $moduleEntries;
 	}
-	
-	protected function getGadgetsModuleEntries() {					
+
+	protected function getGadgetsModuleEntries() {
 		if (Cache::has('module-gadgets')) {
 			$moduleEntries = Cache::get('module-gadgets');
 		} else {
@@ -132,7 +133,7 @@ abstract class BaseController extends Controller {
 								get();
 			Cache::add('module-gadgets', $moduleEntries, $this->cacheTimeLimit);
 		}
-		
-		return $moduleEntries;									
+
+		return $moduleEntries;
 	}
 }
